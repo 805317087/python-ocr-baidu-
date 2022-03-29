895c55fc5cbb4ca606f611bb800fbfdc78c16416
星月狼牙 <52643232+805317087@users.noreply.github.com>
2022-03-29 17:20:39 +0800

    Update baiduocr.py
    删除自身百度OCR请求账户

--- baiduocr.py
+++ baiduocr.py
@@ -1,9 +1,9 @@
 from aip import AipOcr
 
 class BaiduOCR(object):
-    APP_ID = '17010327'
-    API_KEY = 'X2MWCU1LG1PX5H6GAXgdlWD7'
-    SECRET_KEY = 'vz6GZ6TkhSFvY3quqcuC3EG8oEW3kThB'
+    APP_ID = 'xxx'
+    API_KEY = 'xxx'
+    SECRET_KEY = 'xxx'
     client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
     def __init__(self, img):
